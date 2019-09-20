f1fa2853e1c13be876b47a96f280c56888a96680
dariusz.korolczuk <dariusz.korolczuk@redseven.tech>
2019-09-20 12:21:27 +0100

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,79 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Dariusz Korolczuk",
+  role: "Full stack developer",
+  linkedinId: "dariusz-korolczuk",
+  skypeid: "dariusz-korolczuk",
+  email: "dariuszdaroukorolczuk@gmail.com",
+  roleDescription:
+    "I like working with code and AWS, learning new things and my playing bass guitar.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "http://www.linkedin.com/in/dariusz-korolczuk",
+      className: "fa fa-linkedin"
+    },
+    {
+      name: "github",
+      url: "http://github.com/dariuszkorolczukcom/dariuszkorolczukcom",
+      className: "fa fa-github"
+    }
+  ],
+  aboutme:
+    "I am a developer working for a telecommunications service provider. I am a self taught Full Stack Web Developer, currently diving deeper into Java. I believe that to be successful in life, one needs to work on his hobby every day. Mine is coding.",
+  address: "Manchester",
+  education: [
+    {
+      UniversityName: "University of Bialystok",
+      specialization: "Sociology",
+      MonthOfPassing: "-",
+      YearOfPassing: "-",
+      Achievements:
+        "Learned a lot about society and quit the uni on my 5th year to start a job"
+    }
+  ],
+  work: [
+    {
+      CompanyName: "Invosys LTD",
+      specialization: "Developer",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements:
+        "Rebuild legacy PHP router ordering system to Reactjs frontend and golang backend, and then added on-demand email and sms router tracking information."
+    }
+  ],
+  skillsDescription: "Programming Languages",
+  skills: [
+    {
+      skillname: "Go"
+    },
+    {
+      skillname: "JavaScript / Reactjs"
+    },
+    {
+      skillname: "PHP"
+    },
+    {
+      skillname: "Java"
+    },
+    {
+      skillname: "AWS"
+    }
+  ],
+  portfolio: [
+    {
+      name: "PatrykLuczynski.pl",
+      description: "SPA for a freelance musician",
+      imgurl: "images/portfolio/patrykluczynski.png"
+    }
+  ],
+  testimonials: [
+    {
+      description:
+        "A designer knows he has achieved perfection not when there is nothing left to add, but when there is nothing left to take away.",
+      name: "Antoine de Saint-Exupery"
+    }
+  ]
+};
+
+export default resumeData;
